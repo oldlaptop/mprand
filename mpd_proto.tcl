db43d716bf0881cdb82bd95ad613a5120340f655
oldlaptop <oldlaptop654@aol.com>
2018-03-31 15:52:20 -0400

    some assorted cleanup, a prototype gui

--- mpd_proto.tcl
+++ mpd_proto.tcl
@@ -5,10 +5,10 @@ set mpd_sock ""
 # simple error-checking case
 set check_err {if $err {return true} else {return false}}
 
-# construct an array from the common key: value format used by mpd, where value
+# construct a dict from the common key: value format used by mpd, where value
 # may often contain ':' characters and whitespace
-proc carr {arrname inlist} {
-	upvar $arrname arr
+proc cdict {inlist} {
+	set ret ""
 	set len [llength $inlist]
 	foreach elem $inlist\
 	{
@@ -17,8 +17,9 @@ proc carr {arrname inlist} {
 		set pivot [string first : $elem]
 		set key [string range $elem 0 [expr $pivot - 1]]
 		set val [string range $elem [expr $pivot + 2] end]
-		array set arr "\{$key\} \{$val\}"
+		dict set ret $key $val
 	}
+	return $ret
 }
 
 proc connect {host port} {
@@ -28,12 +29,9 @@ proc connect {host port} {
 
 	set protover [gets $mpd_sock]
 
-	if {"{[string range $protover 0 5]}" == "{OK MPD}"}\
-	{
+	if {"{[string range $protover 0 5]}" == "{OK MPD}"} {
 		return [string range $protover 7 end]
-	}\
-	else\
-	{
+	} else {
 		return false
 	}
 }
@@ -46,28 +44,21 @@ proc send_command {cmd {upresponse ""}} {
 	flush $mpd_sock
 	set response [gets $mpd_sock]
 
-	while {1}\
-	{
-		if {[string match "*OK*" $response]}\
-		{
+	while {1} {
+		if {[string match "*OK*" $response]} {
 			log "mpd command $cmd successful" 1
 			log "{response $response}" 2
 
-			if {![string is false $upresponse]}\
-			{
+			if {![string is false $upresponse]} {
 				#elide \nOK
 				uplevel 1 set $upresponse "{[split [string range $response 0 end-3] \n]}"
 			}
 
 			return true
-		}\
-		elseif {[string match "ACK*" $response]}\
-		{
+		} elseif {[string match "ACK*" $response]} {
 			log "mpd error: $cmd failed ($response)" 0
 			return false
-		}\
-		else\
-		{
+		} else {
 			set response "$response\n[gets $mpd_sock]"
 		}
 	}
@@ -78,29 +69,20 @@ proc send_command {cmd {upresponse ""}} {
 proc idle_wait {} {
 	set err [send_command idle response]
 
-	if {$err}\
-	{
+	if {$err} {
 		return $response
-	}\
-	else\
-	{
+	} else {
 		return false
 	}
 }
 
-# rather looks like associative arrays were glued on, doesn't it?
-proc player_status {arrname} {
-	upvar $arrname statarr
+proc player_status {} {
 	set err [send_command "status" response]
 
-	if {$err}\
-	{
+	if {$err} {
 		# construct an array with right-values as indices
-		carr statarr $response
-		return true
-	}\
-	else\
-	{
+		return [cdict $response]
+	} else {
 		return false
 	}
 }
@@ -108,30 +90,22 @@ proc player_status {arrname} {
 proc nsongs {} {
 	set err [send_command "stats" response]
 
-	if {$err}\
-	{
+	if {$err} {
 		return [string range [lsearch -inline $response "songs: *"] 7 end]
-	}\
-	else\
-	{
+	} else {
 		return -1
 	}
 }
 
-proc rnd_song {arrname} {
-	upvar $arrname songinfo
+proc rnd_song {} {
 	set rng [::simulation::random::prng_Discrete [expr [nsongs] -1]]
 	set songnum [$rng]
 
 	set err [send_command "search file \"\" window $songnum:[expr $songnum + 1]" response]
 
-	if {$err}\
-	{
-		carr songinfo $response
-		return true
-	}\
-	else\
-	{
+	if {$err} {
+		return [cdict $response]
+	} else {
 		return false
 	}
 }
@@ -142,6 +116,22 @@ proc consume {val} {
 
 	set err [send_command "consume $val"]
 	{*}$check_err
+}
+
+# Returns a list of dicts, one for each song in the queue
+proc getq {} {
+	set err [send_command "playlistinfo" response]
+	if {$err} {
+		set ret ""
+		set files [lsearch -all $response file:*]
+		set ids [lsearch -all $response Id:*]
+		foreach file $files id $ids {
+			lappend ret [cdict [lrange $response $file $id]]
+		}
+		return $ret
+	} else {
+		return false
+	}
 }
 
 proc clq {} {
@@ -155,13 +145,10 @@ proc enq_song {song} {
 	set song [string map {\" \\\"} $song]
 	set err [send_command "addid \"$song\"" response]
 
-	if {$err}\
-	{
+	if {$err} {
 		set spc [string first ":" $response]
 		return [string range $response $spc end]
-	}\
-	else\
-	{
+	} else {
 		return -1
 	}
 }
@@ -169,5 +156,11 @@ proc enq_song {song} {
 proc play {} {
 	global check_err
 	set err [send_command "play"]
+	{*}$check_err
+}
+
+proc next {} {
+	global check_err
+	set err [send_command "next"]
 	{*}$check_err
 }
